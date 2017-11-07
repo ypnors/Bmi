270cffba29a21acc37a6d544633a2e0b69ac505e
ypnors <godhype@gmail.com>
2017-11-07 11:11:06 +0800

    Onclick

--- app/src/main/java/com/ypnors/bmi/MainActivity.java
+++ app/src/main/java/com/ypnors/bmi/MainActivity.java
@@ -1,26 +1,43 @@
 package com.ypnors.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    String s=new String("abc");
+    View.OnClickListener listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button bHelp = (Button) findViewById(R.id.b_help);
+        bHelp.setOnClickListener(listener);
+
     }
     public void bmi(View view){
        // System.out.println("what???");
         Log.d("MainActivity","testing bmi method");
-        EditText edweight=(EditText) findViewById(R.id.ed_weight);
-        EditText edheight=(EditText) findViewById(R.id.ed_height);
-        float weight =Float.parseFloat(edweight.getText().toString());
-        float height =Float.parseFloat(edheight.getText().toString());
+        EditText edWeight=(EditText) findViewById(R.id.ed_weight);
+        EditText edHeight=(EditText) findViewById(R.id.ed_height);
+        float weight =Float.parseFloat(edWeight.getText().toString());
+        float height =Float.parseFloat(edHeight.getText().toString());
         float bmi=weight/(height*height);
+        new AlertDialog.Builder(this)
+                .setMessage(getString(R.string.your_bmi_is)+bmi)
+                .setTitle(R.string.my_title)
+                .setPositiveButton(R.string.ok,null)
+                .show();
         Log.d("MainActivity",String.valueOf(bmi));
     }
 
